eb5ee475947ac3279dc305b10747e0f6dae93d55
jcm <jeongcm7429@datacommand.kr>
2020-09-03 16:31:53 +0900

    entrypoint.sh - erase unused prometheus flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,4 @@
 
 ####start prometheus
 /bin/prometheus --config.file=/prometheus/cdm-cloud-prometheus.yml \
---storage.tsdb.path=/prometheus \
---web.console.libraries=/usr/share/prometheus/console_libraries \
---web.console.templates=/usr/share/prometheus/consoles
+--storage.tsdb.path=/prometheus
